ae9110be37c7d2b285f52b261be988673c9e119b
Ömer Çelik <celikomr@outlook.com>
2024-08-18 16:15:59 +0300

    feat: Add routes basic endpoints

---
+++ src/Qorpe.Api/Controllers/V1/ReverseProxy/RoutesController.cs
@@ -0,0 +1,39 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Qorpe.Api.Controllers.V1.ReverseProxy;
+
+[Route("api/v{version:apiVersion}/routes")]
+[ApiVersion("1.0")]
+public class RoutesController : BaseController
+{
+    [HttpGet("{id}")]
+    public IActionResult GetRoute(long id)
+    {
+        return Ok(id);
+    }
+
+    [HttpGet]
+    public IActionResult GetRoutes()
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult CreateRoute()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateRoute(long id)
+    {
+        return Ok(id);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteRoute(long id)
+    {
+        return Ok(id);
+    }
+}
